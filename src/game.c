a20fc88c8a31da541c248cdc65ebd7bb7f767596
tetli <182772561+tetli@users.noreply.github.com>
2025-11-27 11:52:37 +0100

    Implement game states and dedicated control switches

--- src/game.c
+++ src/game.c
@@ -19,10 +19,16 @@ int dir = DIR_RIGHT;
 
 int get_input()
 {
-    int SwState = get_sw() & 0b1111;
+    int SwState = get_sw() & 0b111111;
     int dir = DIR_NONE;
     switch (SwState)
     {
+    case 0b100000:
+        dir = RESET_GAME;
+        break;
+    case 0b10000:
+        dir = START_GAME;
+        break;
     case 0b1000:
         dir = DIR_LEFT;
         break;
@@ -199,8 +205,16 @@ void draw_grid_to_screen()
     }
 }
 
+GameState gameState = STATE_START;
+
 void game_init(void)
 {
+    gameState = STATE_START;
+    gameover = 0;
+    score = 0;
+    tailLength = 1;
+    dir = DIR_RIGHT;
+
     fill(0);
     headX = MAP_XWIDTH / 2;
     headY = MAP_YHEIGHT / 2;
@@ -215,37 +229,61 @@ void game_init(void)
 
 void game_tick(void)
 {
-    int input = get_input();
-    if (valid_dir(input))
+    switch (gameState)
     {
-        dir = input;
-    }
-    move_snake();
-
-    check_snake_collision();
-
-    if (check_fruit_collision())
-    {
-        new_fruit_pos();
-        score++;
-        // set_displays(0, score);
-        if (tailLength < MAP_XWIDTH * MAP_YHEIGHT)
+    case STATE_START:
+        fill(0xFF);
+        if (get_input() == START_GAME)
         {
-            tailLength++;
-            tailX[tailLength - 1] = tailX[0];
-            tailY[tailLength - 1] = tailY[0];
+            gameState = STATE_PLAYING;
+            fill(0);
         }
+        break;
+
+    case STATE_PLAYING:
+    {
+        int input = get_input();
+        if (valid_dir(input))
+        {
+            dir = input;
+        }
+        move_snake();
+
+        check_snake_collision();
+
+        if (check_fruit_collision())
+        {
+            new_fruit_pos();
+            score++;
+            // set_displays(0, score);
+            if (tailLength < MAP_XWIDTH * MAP_YHEIGHT)
+            {
+                tailLength++;
+                tailX[tailLength - 1] = tailX[0];
+                tailY[tailLength - 1] = tailY[0];
+            }
+        }
+
+        if (!gameover)
+        {
+            clear_grid();
+            grid_add_fruit();
+            grid_add_snake();
+            draw_grid_to_screen();
+        }
+        else
+        {
+            gameState = STATE_GAMEOVER;
+        }
+        break;
     }
 
-    if (!gameover)
-    {
-        clear_grid();
-        grid_add_fruit();
-        grid_add_snake();
-        draw_grid_to_screen();
-    }
-    else
-    {
-        TIMER_CONTROL &= ~0x1;
+    case STATE_GAMEOVER:
+        fill(0xF800); // Red background for Game Over
+        if (get_input() == RESET_GAME)
+        {
+            game_init();
+        }
+        break;
     }
 }
--- src/game.h
+++ src/game.h
@@ -11,6 +11,15 @@
 #define DIR_DOWN 2
 #define DIR_LEFT 3
 #define DIR_RIGHT 4
+#define START_GAME 5
+#define RESET_GAME 6
+
+typedef enum
+{
+    STATE_START,
+    STATE_PLAYING,
+    STATE_GAMEOVER
+} GameState;
 
 void game_init(void);
 void game_tick(void);
